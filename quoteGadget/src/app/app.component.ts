b28f96fd6e991bd7b7052914309bda871cc6ad67
ShaunHarmon <shaun.harmon@gmail.com>
2018-09-27 13:15:18 -0500

    New Api

--- quoteGadget/src/app/app.component.ts
+++ quoteGadget/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     
     randomNum = -1;
     constructor(private http: HttpClient){
-      this.http.get('https://quotes.schollz.com/random/1.json').subscribe((response:any)=>{  
+      this.http.get('https://talaikis.com/api/quotes').subscribe((response:any)=>{  
    
       this.randomNum = this.getRndInteger(0, response.length)
       this.qouteList = response;
