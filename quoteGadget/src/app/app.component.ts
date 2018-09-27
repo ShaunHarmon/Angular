dafa1ae9cb776aea586523eacfd16e5541082d3c
ShaunHarmon <shaun.harmon@gmail.com>
2018-09-27 08:53:11 -0500

    Remove cross origin proxy

--- quoteGadget/src/app/app.component.ts
+++ quoteGadget/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     
     randomNum = -1;
     constructor(private http: HttpClient){
-      this.http.get('https://crossorigin.me/https://www.forbes.com/forbesapi/thought/get.json?limit=5&start=10&stream=true').subscribe((response:any)=>{  
+      this.http.get('https://www.forbes.com/forbesapi/thought/get.json?limit=5&start=10&stream=true').subscribe((response:any)=>{  
    
       this.randomNum = this.getRndInteger(0, response.length)
       this.qouteList = response;
